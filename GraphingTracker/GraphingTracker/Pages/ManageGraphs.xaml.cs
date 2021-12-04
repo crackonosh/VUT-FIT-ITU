fc6e9f0bf577ae358e4daceb19b225c97e3b6134
lukyy106 <lukasneupauer121@gmail.com>
2021-12-04 04:35:24 +0100

    Grafy progress, zatial nefunguju

--- GraphingTracker/GraphingTracker/Pages/ManageGraphs.xaml.cs
+++ GraphingTracker/GraphingTracker/Pages/ManageGraphs.xaml.cs
@@ -1,15 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using GraphingTracker.Pages;
 using Xamarin.Forms;
 
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+using static GraphingTracker.ManageGraphsViewModel;
+using Microcharts;
+using Microcharts.Forms;
+using Entry = Microcharts.ChartEntry;
+
 namespace GraphingTracker
 {
+
     public partial class ManageGraphs : ContentPage
     {
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry(20)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "ppl",
+                ValueLabel = "20"
+            },
+
+            new Entry(30)
+            {
+                Color = SkiaSharp.SKColor.Parse("#F01493"),
+                Label = "spp",
+                ValueLabel = "30"
+            },
+
+            new Entry(50)
+            {
+                Color = SkiaSharp.SKColor.Parse("#F51493"),
+                Label = "ccl",
+                ValueLabel = "50"
+            }
+        };
         public ManageGraphs()
         {
+
             InitializeComponent();
+
+            //Chart1.Chart = new BarChart { Entries = entries };
+            //Chart2.Chart = new PieChart { Entries = entries };
         }
 
         async void Button_Clicked(System.Object sender, System.EventArgs e)
@@ -17,5 +55,6 @@ namespace GraphingTracker
             await Navigation.PushAsync(new AddGraph());
 
         }
+
     }
 }
---
+++ GraphingTracker/GraphingTracker/ViewModels/AddGraphViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+using Xamarin.Forms;
+using Xamarin.CommunityToolkit;
+using Xamarin.Essentials;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using static GraphingTracker.AddGraphViewModel;
+using System.Linq;
+using GraphingTracker.Pages;
+
+using Microcharts;
+using Microcharts.Forms;
+using Entry = Microcharts.ChartEntry;
+
+using System.Collections.Generic;
+
+
+namespace GraphingTracker
+{
+    /*public class AddGraphViewModel
+    {
+        private GetGraph _graph;
+
+        public class GetGraph
+        {
+            public string Name { get; set; }
+
+            public GetGraph(string name)
+            {
+                Name = name;
+            }
+
+            public GetGraph()
+            {
+
+            }
+
+
+        }
+
+        public GetGraph Graph
+        {
+
+            get { return _graph; }
+            set { _graph = value; OnGraphChaned(); }
+        }
+
+        public AddGraphViewModel()
+        {
+            Graph = new GetGraph();
+
+        }
+
+        public event PropertyChangedEventHandler GraphChanged;
+
+        protected virtual void OnGraphChaned([CallerMemberName] string propertyName = null)
+        {
+            GraphChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+
+
+    */
+
+    public class AddGraphViewModel
+    {
+        private GetGraph _graph;
+
+        public class GetGraph
+        {
+
+            //public List<Entry> Name { get; set; }
+            public Chart Ppl;
+
+            public GetGraph(Chart name) => Ppl = name;
+
+            public GetGraph()
+            {
+
+            }
+
+
+        }
+
+        public GetGraph Graph
+        {
+
+            get { return _graph; }
+            set { _graph = value; OnGraphChaned(); }
+        }
+
+        public AddGraphViewModel()
+        {
+            Graph = new GetGraph();
+
+        }
+
+        public event PropertyChangedEventHandler GraphChanged;
+
+        protected virtual void OnGraphChaned([CallerMemberName] string propertyName = null)
+        {
+            GraphChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    
+
+
+}
